475d10dba76c33ea60349496848c64d2f0b647f7
Go-Morishita <go20040128@gmail.com>
2025-05-02 17:10:20 +0900

    update

--- additive/model_test.py
+++ additive/model_test.py
@@ -10,123 +10,153 @@
 # 次のステップはtichiを導入するか, MWGでやる
 # ☆chainsを理解して実装すべき. 多分ここがtaichiと繋がてくる, 先輩のコードを見るべき
 
+# taichiはできたので, burn-inを実装する
+
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import taichi as ti
 
+# -------------------------- 定数 --------------------------
+OMEGA = 1.0
+SIDE = np.sqrt(OMEGA)
+REJECT_POINT = 0.05
+PROPOSAL_STD = 0.05
+SCALING_FACTOR = 6.0
+GETA = 6.0
+a = 25.0
+b = 0.05
+NUM_PARTICLES = 2
+num_chains = 1000 # 並列化のためのチェーン数
+num_steps = 10000
+STORE_INTERVAL = 100 # サンプリング間隔
+
+K2_SAMPLES = 1000
+
+# -------------------------- Taichi field --------------------------
+# Taichiの初期化
+ti.init(arch=ti.cuda, random_seed=42) # GPUを使用する場合はti.cudaを指定
+# 粒子位置
+particles_positions = ti.Vector.field(2, dtype=ti.f32, shape=(num_chains, NUM_PARTICLES))
+# 提案位置
+proposal_positions = ti.Vector.field(2, dtype=ti.f32, shape=(num_chains, NUM_PARTICLES))
+# 現在の確率
+current_probability = ti.field(dtype=ti.f32, shape=(num_chains))
+# 保存用
+num_records = num_steps // STORE_INTERVAL
+samples = ti.field(dtype=ti.f32, shape=(num_chains, num_records))
+# Kappa2の計算用
+kappa2_values = ti.Vector.field(2, dtype=ti.f32, shape=(K2_SAMPLES))
+
+@ti.func
 def S_function():
     return ((a** 2 + 1)** 2 * (b** 2 + 2 * b + 2)) / (2 * ((a** 2 + 1)** 2 * (b + 1) - (a** 2 + 1) * (b - 1) - 2))
 
+@ti.func
 def C_function():
     return (b - 1) / ((a** 2 + 1) * (b + 1)) + 2 / ((a** 2 + 1)** 2 * (b + 1)) + b** 2 / (2 * (b + 1) * S_function())
 
+@ti.func
 def kappa2(r):
-    return np.exp(- (r - b)) * (- np.cos(a * (r - b)) + C_function())
+    return ti.exp(- (r - b)) * (- ti.cos(a * (r - b)) + C_function())
 
-def cal_kappa2(r, scaling_factor, geta):
-    if r < REJECT_POINT:
-        return -1.0
-    else:
-        return scaling_factor * kappa2(r) + geta
-
-def suppression_term(positions, scaling_factor, geta):
-    n = len(positions)
+@ti.func
+def cal_kappa2(r):
+    return ti.select(r < REJECT_POINT, -1.0, SCALING_FACTOR * kappa2(r) + GETA) # REJECT_POINT以下は0にする
     
+@ti.func
+def cal_sigma(positions, chain: ti.i32): # type: ignore[valid-type]
     sigma = 0.0
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            dr = np.abs(positions[i] - positions[j])
-            dr = np.minimum(dr, OMEGA - dr)
-            r = np.hypot(*dr)
-            sigma += cal_kappa2(r, scaling_factor, geta)
-            
-    return - sigma / (S_function() * n * (n - 1) * 0.5 * (C_function() - 1))
-
-def p_additive(positions, scaling_factor, geta):
-    n = len(positions)
-    
-    sigma = 0.0
-    for i in range(n - 1):
-        for j in range(i + 1, n):
+    for i in range(NUM_PARTICLES - 1):
+        for j in range(i + 1, NUM_PARTICLES):
             # 境界周期条件を考慮して、位置を更新する
-            dr = np.abs(positions[i] - positions[j])
-            dr = np.minimum(dr, OMEGA - dr)
-            r = np.hypot(*dr) # 距離を返す：√(x**2 + y**2)
-            sigma += cal_kappa2(r, scaling_factor, geta)
-    
-    return 1 / np.abs(OMEGA)** n + 1 / np.abs(OMEGA)** (n - 2) * sigma + suppression_term(positions, scaling_factor, geta)
+            dr = ti.abs((positions[chain, i] - positions[chain, j]))
+            dr = ti.min(dr, OMEGA - dr)
+            sigma += cal_kappa2(dr.norm())
+    return sigma
 
-def metrpolis_hastings(num_particles, num_steps, proposal_std, scaling_factor, geta):
-    # 初期化
-    side = np.sqrt(OMEGA)
-    positions = np.random.rand(num_particles, 2) * side
-    samples = []
-    current_p = p_additive(positions, scaling_factor, geta)
-    
-    for step in range(num_steps):
-        proposals = positions + np.random.normal(scale=proposal_std, size=positions.shape)
-        proposals = proposals % side
-        
-        proposal_p = p_additive(proposals, scaling_factor, geta)
-        
-        acceptance_ratio = proposal_p / current_p
-        
-        if np.random.rand() < acceptance_ratio:
-            positions, current_p = proposals, proposal_p
-        
-        if step % 100 == 0:
-            r = np.linalg.norm(positions[0] - positions[1])
-            samples.append(r)
-        
-    return np.array(samples)
+@ti.func
+def suppression_term(positions, chain: ti.i32): # type: ignore[valid-type]       
+    return - cal_sigma(positions, chain) / (S_function() * NUM_PARTICLES * (NUM_PARTICLES - 1) * 0.5 * (C_function() - 1))
 
-if __name__ == "__main__":
+@ti.func
+def p_additive(positions, chain: ti.i32): # type: ignore[valid-type]
+    return 1 / ti.abs(OMEGA)** NUM_PARTICLES + 1 / ti.abs(OMEGA)** (NUM_PARTICLES - 2) * cal_sigma(positions, chain) + suppression_term(positions, chain)
+
+@ti.kernel
+def metrpolis_hastings_onestep(step_idx: ti.i32): # type: ignore[valid-type]
+    for chain in range(num_chains):
+        # 提案位置の生成
+        for particle in ti.static(range(NUM_PARTICLES)): # ti.staticはコンパイラに対して、ループの長さが固定であることを伝える
+            delta = ti.randn() * PROPOSAL_STD
+            position = particles_positions[chain, particle] + ti.Vector([delta, ti.randn()*PROPOSAL_STD])
+            proposal_positions[chain, particle] = (position + SIDE) % SIDE  # 周期境界条件を考慮して位置を更新する
+        
+        proposal_probability = p_additive(proposal_positions, chain)
+        
+        # 受容判定
+        if ti.random() < proposal_probability / current_probability[chain]:
+            for particle in ti.static(range(NUM_PARTICLES)):
+                particles_positions[chain, particle] = proposal_positions[chain, particle]
+            current_probability[chain] = proposal_probability
+        
+        # レコード保存    
+        if step_idx % STORE_INTERVAL == 0:
+            recprded_idx = step_idx // STORE_INTERVAL
+            dist = ti.abs((particles_positions[chain, 0] - particles_positions[chain, 1]))
+            dist = ti.min(dist, SIDE - dist) # 周期境界条件を考慮して距離を更新する
+            samples[chain, recprded_idx] = dist.norm() # 2次元ベクトルのノルムを計算する
+
+@ti.kernel
+def init():
+    for x, y in particles_positions:
+        particles_positions[x, y] = ti.random() * SIDE
+
+@ti.kernel
+def init_k2_curve():
+    for i in range(K2_SAMPLES):
+        r = 0.7 * i / (K2_SAMPLES - 1)       # 0 〜 0.7 の等間隔
+        kappa2_values[i] = ti.Vector([r, cal_kappa2(ti.cast(r, ti.f32))])
+        
+# -------------------------- 初期化 --------------------------
+init()
+
+# --------------------------Metropolis-Hastings法の実行--------------------------
+for step_idx in tqdm(range(num_steps)):
+    metrpolis_hastings_onestep(step_idx)
     
-    # --------------------------乱数のシードを設定--------------------------
-    np.random.seed(42)
-    
-    # --------------------------定数の設定--------------------------
-    OMEGA = 1.0
-    REJECT_POINT = 0.05
-    a = 25.0
-    b = 0.05
-    num_particles = 2
-    num_steps = 1000000
-    proposal_std = 0.05
-    scaling_factor = 5.0
-    geta = 5.0
-    
-    # --------------------------Metropolis-Hastings法の実行--------------------------
-    samples = metrpolis_hastings(num_particles, num_steps, proposal_std, scaling_factor, geta)
-    
-    # --------------------------サンプリング結果のプロット--------------------------
-    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
-    
-    # --------------------------正規化なし--------------------------
-    axes[0].hist(samples, bins=50, density=False, alpha=0.6, label='Samples')
-    axes[0].set_xlim(0, 0.7)
-    axes[0].legend()
-    axes[0].set_title("Distanses between two particles")
-    axes[0].set_xlabel("Distance")
-    axes[0].set_ylabel("Density")
-    
-    # --------------------------正規化あり--------------------------
-    target_function_vectorized = np.vectorize(lambda r: max(cal_kappa2(r, scaling_factor, geta), 0), otypes=[float]) # 無理やり REJECT_POINT 以下を0にしているので改善の余地あり
-    xs = np.linspace(0, 0.7, 1000)
-    ys = target_function_vectorized(xs)
-    
-    # samplesの正規化
-    hist, bin_edges = np.histogram(samples, bins=200, density=True)
-    bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
-    
-    nomalized_samples = hist / bin_centers # 円周効果を取り除き, 真の密度を求める
-    
-    axes[1].bar(bin_centers, nomalized_samples, width=bin_edges[1]-bin_edges[0], alpha=0.6, label='Empirical Density')
-    axes[1].plot(xs, ys, label='K$_2$', color='red')
-    axes[1].set_xlim(0, 0.7)
-    axes[1].legend()
-    axes[1].set_title("Normalized Distanses between two particles")
-    axes[1].set_xlabel("Distance")
-    axes[1].set_ylabel("Normalized Density")
-    
-    plt.tight_layout()
-    plt.show()
+samples_np = samples.to_numpy().reshape(-1)
+
+# --------------------------サンプリング結果のプロット--------------------------
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+
+# --------------------------正規化なし--------------------------
+axes[0].hist(samples_np, bins=50, density=False, alpha=0.6, label='Samples')
+axes[0].set_xlim(0, 0.7)
+axes[0].legend()
+axes[0].set_title("Distanses between two particles")
+axes[0].set_xlabel("Distance")
+axes[0].set_ylabel("Density")
+
+# --------------------------正規化あり--------------------------
+init_k2_curve()
+kappa2_np = kappa2_values.to_numpy()
+xs = kappa2_np[:, 0]
+ys = np.maximum(kappa2_np[:, 1], 0.0)  # 負の値を0に設定
+
+# samplesの正規化
+hist, bin_edges = np.histogram(samples_np, bins=200, density=True)
+bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+
+nomalized_samples = hist / bin_centers # 円周効果を取り除き, 真の密度を求める
+
+axes[1].bar(bin_centers, nomalized_samples, width=bin_edges[1]-bin_edges[0], alpha=0.6, label='Empirical Density')
+axes[1].plot(xs, ys, label='K$_2$', color='red')
+axes[1].set_xlim(0, 0.7)
+axes[1].legend()
+axes[1].set_title("Normalized Distanses between two particles")
+axes[1].set_xlabel("Distance")
+axes[1].set_ylabel("Normalized Density")
+
+plt.tight_layout()
+plt.show()
